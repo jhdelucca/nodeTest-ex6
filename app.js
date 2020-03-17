9165d273b463b45ed297d86d3ac69a8f9a0af4d3
João Henrique Delucca Barros <jhdbarros@hotmail.com>
2020-03-17 14:54:51 -0300

    adicao de cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var productsRouter = require('./routes/products');
@@ -13,6 +14,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+var whitelist = ['https://teste-node-heroku.herokuapp.com', 'http://example2.com']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions));
+
 
 app.use('/', indexRouter);
 app.use('/products', productsRouter);
