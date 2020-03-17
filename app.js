fecf155fc3535082fe61d8898592a8146551b1cb
João Henrique Delucca Barros <jhdbarros@hotmail.com>
2020-03-17 15:01:23 -0300

    adicao de cors

--- app.js
+++ app.js
@@ -14,18 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-var whitelist = ['https://teste-node-heroku.herokuapp.com', 'http://localhost:3000/']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 app.use('/', indexRouter);
